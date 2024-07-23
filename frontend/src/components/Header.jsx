0f30396007e076ef62b6e6f9c4b7acfed9bf60c1
William.Janssen <janssenwilliam02@gmail.com>
2024-07-23 20:11:41 +0200

    logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import Logo from '../assets/logo.png'
 
 import React from 'react'
 
 const Header = () => {
   return (
   <header>
-    <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+    <Navbar bg='primary' variant='dark' expand='md' collapseOnSelect>
       <Container>
         <Navbar.Brand href='/'>
+          <img src={Logo} alt="ProShop" />
           ProShop
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
